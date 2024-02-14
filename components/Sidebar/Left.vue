bbdf69e2a926c5ed93f23dd4c8477d7e120b42fa
Sharmaine Payong <sharmainerpayong@gmail.com>
2024-02-14 22:47:21 +0100

    Initialize a project with vue

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div
+      class="p-2 my-2 hover:bg-blue-500 rounded-full bg-red-300 w-min dark:hover:bg-white/20"
+    >
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoPeep />
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
   postcss: {
     plugins: {
       tailwindcss: {},
